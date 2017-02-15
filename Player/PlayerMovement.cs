810202abb3a591fae39167801fe48a5d614925a2
slichten37 <spencer.lichtenberg@gmail.com>
2017-02-15 16:21:46 -0500

    first files for player movement

---
+++ Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Direction currentDir;
+	Vector2 input;
+	bool isMoving = false;
+	Vector3 startPos;
+	Vector3 endPos;
+	float t;
+
+	public float walkSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isMoving) {
+			input = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+			if (Mathf.Abs (input.x) > input.y)
+				input.y = 0;
+			else
+				input.x = 0;
+
+			if (input != Vector2.zero) {
+				StartCoroutine (Move (transform));
+			}
+		}
+
+	}
+
+	public IEnumorator Move(Transform entity)
+	{
+		isMoving = true;
+
+
+		isMoving = false;
+		yield return 0;
+}
+
+enum Direction
+{
+	North,
+	East,
+	South,
+	West
+}
