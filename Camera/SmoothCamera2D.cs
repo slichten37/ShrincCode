91c705cc58294feded873a8dde9fff42621a588d
slichten37 <spencer.lichtenberg@gmail.com>
2017-02-17 14:24:41 -0500

    Camera now follows Player

---
+++ Camera/SmoothCamera2D.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera2D : MonoBehaviour
+{
+    public Transform player;
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, -10); // Camera follows the player but 6 to the right
+    }
+}
