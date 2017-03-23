4eecd7db696a45e04302d80d32050c665e5e0397
slichten37 <spencer.lichtenberg@gmail.com>
2017-03-23 17:54:17 -0400

    added doors

---
+++ Player/OpenDoor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    public GameObject door;
+    public AlertMovement alert;
+
+    public bool waitForButtonPress;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        alert = FindObjectOfType<AlertMovement>();
+        waitForButtonPress = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitForButtonPress)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                door.GetComponent<Renderer>().enabled = false;
+                door.GetComponent<BoxCollider2D>().enabled = false;
+            }
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            waitForButtonPress = true;
+            alert.GetComponent<Renderer>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            waitForButtonPress = false;
+            alert.GetComponent<Renderer>().enabled = false;
+            door.GetComponent<Renderer>().enabled = true;
+            door.GetComponent<BoxCollider2D>().enabled = true;
+
+        }
+    }
+
+}
