0d9c1ab47c089680d763b808b01b4baca2acb7b7
slichten37 <spencer.lichtenberg@gmail.com>
2017-03-23 20:07:25 -0400

    locked camera on map

--- Camera/SmoothCamera2D.cs
+++ Camera/SmoothCamera2D.cs
@@ -6,6 +6,24 @@ public class SmoothCamera2D : MonoBehaviour
     public Transform player;
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, -10); // Camera follows the player but 6 to the right
+        float newX = player.position.x;
+        float newY = player.position.y;
+        if (newY > 4)
+        {
+            newY = 4f;
+        }
+        if (newY < -12.5)
+        {
+            newY = -12.5f;
+        }
+        if (newX > -30.6)
+        {
+            newX = -30.6f;
+        }
+        if (newX < -40.5)
+        {
+            newX = -40.5f;
+        }
+        transform.position = new Vector3(newX, newY, -10); // Camera follows the player but 6 to the right
     }
 }
